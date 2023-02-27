c434fa162a53364aa637ca683f58f35549001e2c
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-02-27 14:10:46 +0530

    Added solution - LeetHub

---
+++ Reverse both parts - GFG/reverse-both-parts.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList()
+{
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    int data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+Definition for singly Link List Node
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+public:
+Node* reversed(Node* head,Node* done)
+{
+    if(head->next==done)
+    return head;
+    Node* temp=reversed(head->next,done);
+head->next->next=head;
+head->next=NULL;
+return temp;
+}
+   
+    Node *reverse(Node *head, int k)
+    {
+      Node* temp=head;
+      while(k-- && temp!=NULL)
+      {
+          temp=temp->next;
+      }
+      Node* ans1=reversed(head,temp);
+      Node* ans2=reversed(temp,NULL);
+      head->next=ans2;
+      return ans1;
+      
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        struct Node *head = inputList();
+        int k;
+        cin >> k;
+
+        Solution obj;
+        Node *res = obj.reverse(head, k);
+
+        printList(res);
+    }
+}
+// } Driver Code Ends
