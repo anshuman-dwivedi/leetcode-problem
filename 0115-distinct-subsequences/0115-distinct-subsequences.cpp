5a3a572fd1045465d105dd2ae91e476be9ae219e
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-05-20 00:07:09 +0530

    Time: 32 ms (83.96%), Space: 6.4 MB (90.91%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,21 +3,20 @@ public:
     int numDistinct(string s, string t) {
         int m=s.length();
         int n=t.length();
-        vector<vector<double>>dp(m+1,vector<double>(n+1,0));
-        for(int i=0;i<=m;i++)
-        {
-         dp[i][0]=1;   
-}
+        vector<double>prev(n+1,0),curr(n+1,0);
+     prev[0]=curr[0]=1;
         for(int i=1;i<=m;i++)
         {
             for(int j=1;j<=n;j++)
             {
                 if(s[i-1]==t[j-1])
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                  curr[j]=prev[j-1]+prev[j];
                 else
-                    dp[i][j]=dp[i-1][j];
+                   curr[j]=prev[j];
+
+           }
+             prev=curr;
 }
-}
-     return dp[m][n];
+     return (int)prev[n];
     }
 };
