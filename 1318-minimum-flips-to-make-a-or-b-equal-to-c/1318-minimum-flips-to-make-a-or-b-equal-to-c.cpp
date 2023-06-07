d1411af893c85fb333c0227634635627a6703e2d
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-06-07 23:16:43 +0530

    Time: 3 ms (40.05%), Space: 5.9 MB (93.33%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ans=0;
+     while(a|| b|| c)
+     {
+         int bit_a=a&1;
+          int bit_b=b&1;
+          int bit_c=c&1;
+         if(bit_c==0)
+         {
+             ans+=bit_a+bit_b;
+}
+         else{
+             if(bit_a==0 && bit_b==0)
+                 ans+=1;
+}
+       a>>=1;
+         b>>=1;
+         c>>=1;
+}
+  return ans;      
+  }
+};
