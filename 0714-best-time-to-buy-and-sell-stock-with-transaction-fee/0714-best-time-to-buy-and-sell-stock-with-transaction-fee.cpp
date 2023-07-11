7caddd2583b4366f890d05c48f957e43afc7a946
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-07-12 00:36:41 +0530

    Time: 233 ms (41.55%), Space: 90.8 MB (35.25%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -6,15 +6,10 @@ public:
         dp[n][0]=dp[n][1]=0;
         for(int i=n-1;i>=0;i--)
         {
-            for(int j=0;j<=1;j++)
-            {
-                if(j)
-                {
-                    dp[i][j]=max(-p[i]+dp[i+1][0],0+dp[i+1][1]);
-}
-                else
-                 dp[i][j]=max(p[i]-fee+dp[i+1][1],0+dp[i+1][0]);   
-}
+          
+                    dp[i][1]=max(-p[i]+dp[i+1][0],0+dp[i+1][1]);
+
+                 dp[i][0]=max(p[i]-fee+dp[i+1][1],0+dp[i+1][0]);   
 }
        return dp[0][1];
     }
