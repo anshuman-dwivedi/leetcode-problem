597db2187101adee1d1189c85dcac6a093986077
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-07-22 13:07:57 +0530

    Time: 28 ms (38.98%), Space: 9.2 MB (34.22%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     double knightProbability(int N, int steps, int start_x, int start_y) {
-         vector<vector<double>>dp(N,vector<double>(N,0.0));
-	   dp[start_x][start_y]=1.0;
-	   while(steps--)
-	   {
+         vector<vector<vector<double>>>dp(steps+1,vector<vector<double>>(N,vector<double>(N,0.0)));
+	   dp[0][start_x][start_y]=1.0;
 	       int dirx[]={-2,2,-1,1,-2,2,-1,1};
 	       int diry[]={1,1,2,2,-1,-1,-2,-2};
-	       vector<vector<double>>ndp(N,vector<double>(N,0.0));
+            for(int k=1;k<=steps;k++){
 	       for(int i=0;i<N;i++)
 	       {
 	           for(int j=0;j<N;j++)
 	           {
-	             for(int k=0;k<8;k++)
+	             for(int dir=0;dir<8;dir++)
 	             {
-	             int newx=i+dirx[k];
-	             int newy=j+diry[k];
-	             if(dp[i][j]!=0)
-	             {
+	             int newx=i+dirx[dir];
+	             int newy=j+diry[dir];
 	             if(newx>=0 && newy>=0 && newx<N && newy<N)
-	                 ndp[newx][newy]+=dp[i][j]/8.0;
-	             }
+	                 dp[k][i][j]+=(dp[k-1][newx][newy]/8.0);
 	             }
 	           }
-	       }
-	       dp=ndp;
 	   }
+            }
 	   double ans=0.0;
+        
 	   for(int i=0;i<N;i++)
 	   {
 	       for(int j=0;j<N;j++)
 	       {
-	           ans+=dp[i][j];
+	           ans+=dp[steps][i][j];
 	       }
 	   }
 	   return ans; 
