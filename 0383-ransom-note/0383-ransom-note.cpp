857e2fc6df18915b73343f48b75036db6e3a8541
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-07-25 12:26:29 +0530

    Time: 23 ms (50.02%), Space: 8.9 MB (29.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int>m;
-        int count=0;
-        for(int i=0;i<ransomNote.length();i++)
-        {
-            m[ransomNote[i]]++;
-}
         for(int i=0;i<magazine.length();i++)
         {
-            if(m.find(magazine[i])!=m.end())
-            {
-                m[magazine[i]]--;
+            m[magazine[i]]++;
 }
-}
-        for(auto x:m)
+        for(int i=0;i<ransomNote.length();i++)
         {
-            if(x.second<=0)
-                count++;
+          if(--m[ransomNote[i]]<0)
+              return false;
 }
-        return count==m.size();
-        
+     return true;
     }
 };
