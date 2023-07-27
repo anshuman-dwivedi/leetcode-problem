593af0b53c3275f24608e0a055ea2d011b1b2538
Anshuman Dwivedi <92855493+anshuman-dwivedi@users.noreply.github.com>
2023-07-27 16:34:17 +0530

    Time: 45 ms (47.46%), Space: 32.5 MB (46.59%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -12,13 +12,34 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-    if(!root1)
-        return root2;
-        if(!root2)
-            return root1;
-        root1->val+=root2->val;
-        root1->left=mergeTrees(root1->left,root2->left);
-        root1->right=mergeTrees(root1->right,root2->right);
+        if(root1==NULL && root2==NULL)
+            return NULL;
+        if(!root1)
+            return root2;
+    if(!root2)
+        return root1;
+   queue<pair<TreeNode*,TreeNode*>>q;
+        q.push({root1,root2});
+        while(!q.empty())
+        {
+            int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* temp1=q.front().first;
+                TreeNode* temp2=q.front().second;
+                q.pop();
+                if(temp2!=NULL)
+                temp1->val+=temp2->val;
+                if(temp1->left && temp2->left)
+                    q.push({temp1->left,temp2->left});
+                else if(temp2->left)
+                    temp1->left=temp2->left;
+                if(temp1->right && temp2->right)
+                    q.push({temp1->right,temp2->right});\
+                    else if(temp2->right)
+                        temp1->right=temp2->right;
+}
+}
         return root1;
     }
 };
